cd2cfdc3e756d96dbba3bdf05227f1c9af5c60ae
Muhammed Rajab <muhammedrajab101@gmail.com>
2025-04-22 21:41:32 +0530

    initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#define LOG std::cout
+#define NL "\n"
+
+int main()
+{
+    /*
+    =------------=
+    | telnet.cpp |
+    =------------=
+
+    wanna show cool graphics via TCP? here you got it!
+    */
+    LOG << "HELLO WORLD!" << NL;
+    return EXIT_SUCCESS;
+}
